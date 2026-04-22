fc018e3dca3efb016be226313d7180a488068da3
Thomas Pedneault <thomas@pedneault.dev>
2026-04-22 17:47:51 -0400

    Add completion stack and ignore Elpaca state

--- modules/mod-completion.el
+++ modules/mod-completion.el
@@ -1,4 +1,44 @@
-;;; mod-completion.el --- Completion module placeholder -*- lexical-binding: t; -*-
+;;; mod-completion.el --- Completion and navigation foundation -*- lexical-binding: t; -*-
+
+(use-package vertico
+  :ensure t
+  :demand t
+  :config
+  (setq vertico-cycle t)
+  (vertico-mode 1))
+
+(use-package orderless
+  :ensure t
+  :demand t
+  :config
+  (setq completion-styles '(orderless basic)
+        completion-category-defaults nil
+        completion-category-overrides '((file (styles basic partial-completion)))))
+
+(use-package marginalia
+  :ensure t
+  :after vertico
+  :config
+  (marginalia-mode 1))
+
+(use-package consult
+  :ensure t
+  :demand t
+  :config
+  ;; Keep the existing leader tree and upgrade the underlying buffer switcher.
+  (global-set-key [remap switch-to-buffer] #'consult-buffer)
+  (setq consult-preview-key nil))
+
+(use-package corfu
+  :ensure t
+  :demand t
+  :config
+  (setq corfu-auto nil
+        corfu-cycle t
+        corfu-preselect 'prompt)
+  (global-corfu-mode 1))
+
+(savehist-mode 1)
 
 (provide 'mod-completion)
 
