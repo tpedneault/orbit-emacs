063d75089246a3cd5ee5026ebbd99c1e7cd10e40
Thomas Pedneault <thomas@pedneault.dev>
2026-04-22 22:16:05 -0400

    Add Evil surround, commentary, args, and multicursor support

--- modules/mod-evil.el
+++ modules/mod-evil.el
@@ -16,6 +16,33 @@
   :config
   (evil-collection-init))
 
+(use-package evil-surround
+  :ensure t
+  :after evil
+  :config
+  (global-evil-surround-mode 1))
+
+(use-package evil-commentary
+  :ensure (:build (:not elpaca-build-compile))
+  :after evil
+  :config
+  (evil-commentary-mode 1))
+
+(use-package evil-args
+  :ensure (:fetcher github
+           :repo "wcsmith/evil-args"
+           :main-file "evil-args.el")
+  :after evil
+  :config
+  (define-key evil-inner-text-objects-map "a" #'evil-inner-arg)
+  (define-key evil-outer-text-objects-map "a" #'evil-outer-arg))
+
+(use-package evil-mc
+  :ensure t
+  :after evil
+  :config
+  (global-evil-mc-mode 1))
+
 (provide 'mod-evil)
 
 ;;; mod-evil.el ends here
